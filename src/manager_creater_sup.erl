1b9d1e90e7b9fe6285fc8067351256f7e39d7f90
wangzihao223 <1600964393@qq.com>
2023-02-13 20:20:29 +0800

    添加监控树

---
+++ src/manager_creater_sup.erl
@@ -0,0 +1,35 @@
+-module(manager_creater_sup).
+
+-behaviour(supervisor).
+
+-export([start_link/0]).
+
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%% sup_flags() = #{strategy => strategy(),         % optional
+%%                 intensity => non_neg_integer(), % optional
+%%                 period => pos_integer()}        % optional
+%% child_spec() = #{id => child_id(),       % mandatory
+%%                  start => mfargs(),      % mandatory
+%%                  restart => restart(),   % optional
+%%                  shutdown => shutdown(), % optional
+%%                  type => worker(),       % optional
+%%                  modules => modules()}   % optional
+init([]) ->
+    SupFlags = #{strategy => one_for_one,
+                 intensity => 1,
+                 period => 10},
+    ChildSpecs = [
+        #{
+            id => manager_creater,
+            start => {manager_creater, start_link, []}
+        }
+    ],
+    {ok, {SupFlags, ChildSpecs}}.
+
+%% internal functions
