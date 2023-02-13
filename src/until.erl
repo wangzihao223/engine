74d4f9e4fe16afd785951c492ac3ddc3f2fe0950
wangzihao223 <1600964393@qq.com>
2023-02-13 20:24:33 +0800

    更换 uuid 库

--- src/until.erl
+++ src/until.erl
@@ -6,3 +6,13 @@ sets_equal(Set1, Set2) ->
     sets:is_subset(Set1, Set2) andalso
     sets:is_subset(Set2, Set1).
 
+
+% error catch
+error_catcher(F, Args) ->
+    try apply(F, Args) of
+        Val -> Val
+    catch
+        throw:X -> {thrown, X};
+        exit:X -> {exited, X};
+        error:X -> {error, X}
+    end.
--- src/uuid.hrl
+++ src/uuid.hrl
@@ -1,9 +1,17 @@
-%-*-Mode:erlang;coding:utf-8;tab-width:4;c-basic-offset:4;indent-tabs-mode:()-*-
-% ex: set ft=erlang fenc=utf-8 sts=4 ts=4 sw=4 et nomod:
+-define(VARIANT10, 2#10).
 
-%%% provide the typical uuid namespaces
--define(UUID_NAMESPACE_DNS,  <<16#6ba7b8109dad11d180b400c04fd430c8:128>>).
--define(UUID_NAMESPACE_URL,  <<16#6ba7b8119dad11d180b400c04fd430c8:128>>).
--define(UUID_NAMESPACE_OID,  <<16#6ba7b8129dad11d180b400c04fd430c8:128>>).
--define(UUID_NAMESPACE_X500, <<16#6ba7b8149dad11d180b400c04fd430c8:128>>).
 
+%% For 100 nanosecond interval transformation in UUIDv1.
+
+%% Offset between 15 October 1582 and 1 January 1970
+-define(nanosecond_intervals_offset, 122192928000000000).
+
+%% microseconds to nanoseconds
+-define(nanosecond_intervals_factor, 10).
+
+
+%% Version
+-define(UUIDv1, 1).
+-define(UUIDv3, 3).
+-define(UUIDv4, 4).
+-define(UUIDv5, 5).
