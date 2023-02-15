9a24705f2e4598799f62baf5ba0c58c94dc221db
wangzihao223 <1600964393@qq.com>
2023-02-15 14:17:55 +0800

    添加任务管理服务

---
+++ src/task_server.erl
@@ -0,0 +1,25 @@
+-module(task_server).
+
+-behaviour(gen_server).
+
+-export([start_link/0]).
+
+-export([init/1]).
+-export([handle_call/3]).
+-export([handle_cast/2]).
+
+-define(SERVER, ?MODULE).
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+init([]) ->
+    Table = ets:new(?MODULE, [set, protected]), 
+    {ok,Table}.
+
+handle_cast({<<"proxy_group">>, Manager, SidList, MaxStep}, _Table)->
+    proxy_process:create_group_process(Manager,
+        SidList, MaxStep).
+handle_call({<<"new_proxy">>, Manager, Sid, Step, MaxStep}, 
+    _From, _Table) ->
+    proxy_process:new_process(Manager, Sid, Step, MaxStep).
